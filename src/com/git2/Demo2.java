0786291ecbe3cac8016c53a7e2446d5f48fc29b9
Kumarayoningithub <kumarayoninmail@gmail.com>
2026-03-09 14:40:12 +0530

    Initial commit for Project2 Jenkins

---
+++ src/com/git2/Demo2.java
@@ -0,0 +1,14 @@
+package com.git2;
+
+public class Demo2 {
+   public static void main(String[] args) {
+	
+	   int number = 15;
+
+       if (number % 2 == 0) {
+           System.out.println(number + " is Even");
+       } else {
+           System.out.println(number + " is Odd");
+       }
+ }
+}
